3ad20cbfb1187818c64aedbe1ab2573e96aa64a8
Sssandra27 <62666165+Pectiine@users.noreply.github.com>
2020-06-14 23:16:02 +0200

    premier

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+$pages = scandir('pages /');
+if (isset($_GET['page']) && !empty($_GET['page'])) {
+    if (in_array($_GET['page'] . '.php', $pages)) {
+        $page = $_GET['page'];
+    } else {
+        $page = "error";
+    }
+} else {
+    $page = "home";
+}
+
+$pages_functions =scandir('functions/');
+if(in_array($page.'.func.php' ,$pages_functions)) {
+    include 'functions/'.$page.'func.php'; 
+}
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
+
+<body>
+
+    <div class="container">
+
+        <? php
+                inclure  «pages /» . $page . '.php' ;
+            ?>
+
+    </div>
+
+    <!--JavaScript at end of body for optimized loading-->
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+</body>
+
+</html>
---
+++ pages/error.php
@@ -0,0 +1,2 @@
+<h2> aie aie aie </h2> 
+<p class="flow-tex"> Cette page n'existe pas</p>
