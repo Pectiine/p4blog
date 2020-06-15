1eb4deefde3bd244836b9b560fe30cb91e5799ad
Sssandra27 <62666165+Pectiine@users.noreply.github.com>
2020-06-15 10:39:12 +0200

    trois

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 
  include 'function/main-functions.php';
-
 $pages = scandir('pages /');
 if (isset($_GET['page']) && !empty($_GET['page'])) {
     if (in_array($_GET['page'] . '.php', $pages)) {
@@ -12,14 +11,11 @@ if (isset($_GET['page']) && !empty($_GET['page'])) {
 } else {
     $page = "home";
 }
-
 $pages_functions =scandir('function/');
 if(in_array($page.'.func.php' ,$pages_functions)) {
     include 'function/'.$page.'.func.php'; 
 }
 ?>
-
-
 <!DOCTYPE html>
 <html>
 
@@ -28,21 +24,16 @@ if(in_array($page.'.func.php' ,$pages_functions)) {
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!--Import materialize.css-->
     <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
-
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
 
 <body>
-
     <div class="container">
-
         <? php
                 inclure  «pages/» . $page . '.php' ;
             ?>
-
     </div>
-
     <!--JavaScript at end of body for optimized loading-->
     <script type="text/javascript" src="js/materialize.min.js"></script>
 </body>
