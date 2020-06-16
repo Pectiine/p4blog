a35b35ee3cd99d52ce2230e75b38174cca514fd6
Sssandra27 <62666165+Pectiine@users.noreply.github.com>
2020-06-16 14:51:38 +0200

    changement pour mvc

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
  include 'function/main-functions.php';
 $pages = scandir('pages /');
 if (isset($_GET['page']) && !empty($_GET['page'])) {
@@ -15,26 +16,3 @@ if(in_array($page.'.func.php' ,$pages_functions)) {
     include 'function/'.$page.'.func.php'; 
 }
 ?>
-<!DOCTYPE html>
-<html>
-
-<head>
-    <!--Import Google Icon Font-->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <!--Import materialize.css-->
-    <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
-    <!--Let browser know website is optimized for mobile-->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-</head>
-
-<body>
-    <div class="container">
-        <? php
-                inclure  «pages/» . $page . '.php' ;
-            ?>
-    </div>
-    <!--JavaScript at end of body for optimized loading-->
-    <script type="text/javascript" src="js/materialize.min.js"></script>
-</body>
-
-</html>
