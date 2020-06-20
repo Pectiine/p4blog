afa21580df542a1d309594519bd026f6b2451f2d
Sssandra27 <62666165+Pectiine@users.noreply.github.com>
2020-06-20 15:19:54 +0200

    index

--- index.php
+++ index.php
@@ -1,18 +1,107 @@
 <?php
+session_start();
+require('controller/controller.php');
+require('controller/PostController.php');
 
- include 'function/main-functions.php';
-$pages = scandir('pages /');
-if (isset($_GET['page']) && !empty($_GET['page'])) {
-    if (in_array($_GET['page'] . '.php', $pages)) {
-        $page = $_GET['page'];
-    } else {
-        $page = "error";
+$postsController = new www\P4\controller\PostController();
+
+if (isset($_GET['action'])) {
+
+    switch ($_GET['action']) {
+        case 'index':
+            $postsController->index();
+            break;
+
+        case 'post':
+            if (isset($_GET['post']) && $_GET['post'] > 0) {
+                $postsController->post($_GET['post']);
+            } else {
+                echo 'Erreur : Aucun identifiant de post envoyé';
+            }
+            break;
+        case 'posts':
+            $postsController->posts();
+            break;
+        case 'add_post':
+            $postsController->addPost();
+            break;
+        case 'formAddPost':
+            if (!empty($_POST['title_post']) && !empty($_POST['text_post'])) {
+                $postsController->formAddPost($_POST['title_post'], $_SESSION['username'], $_POST['text_post']);
+            } else {
+                echo "Erreur : tous les champs ne sont pas remplis !";
+            }
+            break;
+        case 'deletePost':
+            if (isset($_GET['post']) && $_GET['post'] > 0) {
+                $postsController->deletePost($_GET['post']);
+            } else {
+                echo 'Erreur : Aucun identifiant de post envoyé';
+            }
+            break;
+        case 'updatePost':
+            if (isset($_GET['post']) && $_GET['post'] > 0) {
+                $postsController->updatePost($_GET['post']);
+            } else {
+                echo 'Erreur : Aucun identifiant de post envoyé';
+            }
+            break;
+        case 'formUpdatePost':
+            if (isset($_GET['post']) && $_GET['post'] > 0) {
+                if (!empty($_POST['title_post']) && !empty($_POST['text_post'])) {
+                    $postsController->formUpdatePost($_GET['post'], $_POST['title_post'], $_POST['text_post']);
+                } else {
+                    echo "Erreur : tous les champs ne sont pas remplis !";
+                }
+            } else {
+                echo 'Erreur : Aucun identifiant de post envoyé';
+            }
+            break;
+
+        case 'addComment':
+            if (isset($_GET['post']) && $_GET['post'] > 0) {
+                if (!empty($_POST['username']) && !empty($_POST['text_comment'])) {
+                    addComment($_GET['post'], $_POST['username'], $_POST['text_comment']);
+                } else {
+                    echo "Erreur : tous les champs ne sont pas remplis !";
+                }
+            } else {
+                echo "Erreur : aucun identifiant de billet envoyé";
+            }
+            break;
+        case 'deleteComment':
+            if (isset($_GET['post']) && $_GET['post'] > 0 && isset($_GET['comment']) && $_GET['comment'] > 0) {
+                deleteComment($_GET['post'], $_GET['comment']);
+            } else {
+                echo 'Erreur : Aucun identifiant de post ou de commentaire envoyé';
+            }
+            break;
+
+        case 'report_comment':
+            report_comment();
+            break;
+        case 'form_report':
+            form_report();
+            break;
+
+        case 'management':
+            management();
+            break;
+
+        case 'connect':
+            connect();
+            break;
+        case 'formConnect':
+            if (!empty($_POST['username']) && !empty($_POST['password'])) {
+                formConnect();
+            } else {
+                echo "Erreur : tous les champs ne sont pas remplis !";
+            }
+            break;
+        case 'disconnect':
+            disconnect();
+            break;
     }
 } else {
-    $page = "home";
+    $postsController->index();
 }
-$pages_functions =scandir('function/');
-if(in_array($page.'.func.php' ,$pages_functions)) {
-    include 'function/'.$page.'.func.php'; 
-}
-?>
